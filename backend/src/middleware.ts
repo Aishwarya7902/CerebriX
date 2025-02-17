214d463b41e6b34dcd4698ce65184d5b6933ff86
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2025-02-18 00:13:15 +0530

    Added user middleware

---
+++ backend/src/middleware.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from "express";
+import jwt, { JwtPayload } from 'jsonwebtoken'
+
+// Extend Express Request type to include userId
+interface CustomRequest extends Request {
+    userId?: string; // Optional in case JWT is invalid
+}
+
+export const userMiddleware = (req: CustomRequest, res: Response, next: NextFunction) => {
+    const header = req.headers["authorization"];
+    const decoded = jwt.verify(header as string, process.env.JWT_SECRET as string)
+
+    if (decoded) {
+        if (typeof decoded === "string") {
+            res.status(403).json({
+                message: "You are not logged in"
+            })
+
+            return;
+        }
+
+        req.userId = (decoded as JwtPayload).id;
+        next()
+    }
+    else{
+        res.status(403).json({
+            message:"You are not logged in"
+        })
+    }
+}
