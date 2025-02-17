3805a9a6cdd5bdfe01e20c0a1f719443156d6a1c
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2025-02-17 23:13:21 +0530

    ContentSchema added

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,6 +2,7 @@
 
 import {model, Model,Schema} from "mongoose";
 import mongoose from "mongoose";
+import { title } from "process";
 mongoose.connect(process.env.DB_CONNECT as string)
 
   /************************************  DB CONNECTION DONE ***********************************************/
@@ -14,3 +15,13 @@ mongoose.connect(process.env.DB_CONNECT as string)
 
   export const UserModel= model('User',UserSchema)
   
+
+const ContentSchema=new Schema({
+    title:String,
+    link:String,
+    tags:[{type:mongoose.Types.ObjectId,ref:'Tag'}],
+    type:String,
+    userId:{type:mongoose.Types.ObjectId,ref:'User',required:true}
+})
+
+export const ContentModel=model('Content',ContentSchema)
