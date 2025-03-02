8ac260ff3858e39c7b56156e57dddc21fb25e1a5
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2025-03-02 21:19:31 +0530

    re initialized frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,68 +1,10 @@
-import './App.css'
-import { Button } from './components/ui/Button'
-import { PlusIcon } from './icons/PlusIcon'
-import { ShareIcon } from './icons/ShareIcon'
 
 function App() {
 
 
-  return (
-    <>
-
-      <Button
-        startIcon={<PlusIcon size='lg' />}
-        endIcon={<ShareIcon size='lg' />}
-        size='lg'
-        title={"Share"}
-        variant='secondary'
-      />
-
-      <Button
-        startIcon={<PlusIcon size='lg' />}
-        endIcon={<ShareIcon size='lg' />}
-        size='lg'
-        title={"Share"}
-        variant='primary'
-      />
-
-      <Button
-        startIcon={<PlusIcon size='md' />}
-        endIcon={<ShareIcon size='md' />}
-        size='md'
-        title={"Share"}
-        variant='secondary'
-      />
-
-      <Button
-        startIcon={<PlusIcon size='md' />}
-        endIcon={<ShareIcon size='md' />}
-        size='md'
-        title={"Share"}
-        variant='primary'
-      />
-
-      <Button
-        startIcon={<PlusIcon size='sm' />}
-        endIcon={<ShareIcon size='sm' />}
-        size='sm'
-        title={"Share"}
-        variant='secondary'
-      />
-
-      <Button
-        startIcon={<PlusIcon size='sm' />}
-        endIcon={<ShareIcon size='sm' />}
-        size='sm'
-        title={"Share"}
-        variant='primary'
-      />
-
-
-
-
-
-    </>
-  )
+  return <div>
+        Hi there
+  </div>
 }
 
 export default App
