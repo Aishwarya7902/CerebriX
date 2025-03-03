94f7f2253eb1d14ba33339b65ed7b9775badd00b
Aishwarya7902 <striver.aish10j20a9720@gmail.com>
2025-03-04 01:31:35 +0530

    Added a generic card component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
 import { Button } from "./components/Button"
+import { Card } from "./components/Card"
 import { PlusIcon } from "./icons/PlusIcon"
 import { ShareIcon } from "./icons/ShareIcon"
 
 function App() {
 
 
-  return <div >
-       <Button variant="primary" text="Add Content" startIcon={<PlusIcon/>}/>
-       <Button variant="secondary" text="Share brain" startIcon={<ShareIcon/>}/>
-       
+  return <div className="p-4">
+    <div className="flex justify-end gap-4">
+      <Button variant="primary" text="Add Content" startIcon={<PlusIcon />} />
+      <Button variant="secondary" text="Share brain" startIcon={<ShareIcon />} />
+    </div>
+    <div className="flex gap-4">
+      <Card title="First tweet" type="twitter" link="https://twitter.com/Abhishekcur/status/1896627058253701316" />
+
+      <Card title="Harkirat Motivation" type="youtube" link="https://www.youtube.com/watch?v=fZGnOPnvBp8" />
+    </div>
+
   </div>
 }
 
